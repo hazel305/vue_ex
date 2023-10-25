b2103b3c1782ca83c43c3c52f1a158d19e2231b1
hazel305 <hazel413@naver.com>
2023-10-25 12:22:25 +0900

    [FEAT]event handling

--- vue-project/src/views/ListView.vue
+++ vue-project/src/views/ListView.vue
@@ -1,21 +1,19 @@
 <script>
-// const awesome = true;
-
 export default {
   data() {
     return {
-      ParentLangs: "Web",
+      parentLangs: "Web",
       items: [
-        { id: 1, title: "HTML", desc: "HTML is..." },
-        { id: 2, title: "CSS", desc: "CSS is..." },
-        { id: 3, title: "JS", desc: "JS is..." },
+        { id: 1, title: "HTML", desc: "HTML ..." },
+        { id: 2, title: "CSS", desc: "CSS ..." },
+        { id: 3, title: "Javascript", desc: "Javascript ..." },
       ],
       myObject: {
         title: "Vue에서 목록을 작성하는 방법",
         author: "홍길동",
         publishedAt: "2016-04-10",
       },
-      numbers: [1, 2, 3, 4, 5],
+      numbers: [1, 2, 3, 4, 5, 6],
       sets: [
         [1, 2, 3, 4, 5],
         [6, 7, 8, 9, 10],
@@ -24,16 +22,15 @@ export default {
   },
   computed: {
     evenNumbers() {
-      console.log("evenNumbers 실행!");
-      //numbers의 값이 변경될때만 실행
+      console.log("evenNumbers 실행");
+      //numbers의 변경될 경우만 실행
       return this.numbers.filter((n) => n % 2 === 0);
     },
   },
   methods: {
     even(numbers) {
-      console.log("even 실행!");
-      //인수 numbers의 값을 가지고 직접 바뀌지 않으면 sets값이 바뀌는거에 상관없이
-      //data 함수내 어느값이든 변경되면 실행
+      console.log("even 실행");
+      //data함수내 어느 값이든 변경되면 실행
       return numbers.filter((number) => number % 2 === 0);
     },
   },
@@ -42,45 +39,37 @@ export default {
 
 <template>
   <div class="layout">
-    <h1>This is list Rendering page</h1>
+    <h1>This is List Rendering page</h1>
 
     <ul>
       <li v-for="(item, index) in items" :key="item.id">
-        {{ index }} / {{ item.title }}
+        {{ parentLangs }}: {{ index }}-{{ item.title }}
       </li>
     </ul>
     <ul>
-      <li v-for="value in myObject" :key="value.id">
+      <li v-for="value in myObject" :key="value">
         {{ value }}
       </li>
     </ul>
+    <hr />
     <span v-for="n in 10" :key="n">{{ n }}</span>
-
+    <hr />
     <ul>
-      <hr />
       <template v-for="item in items" :key="item.id">
         <li>{{ item.desc }}</li>
       </template>
     </ul>
-
-    <hr />
-    <h3>필터링/정렬 결과 표시</h3>
+    <h2>필터</h2>
     <ul>
-      <li v-for="n in evenNumbers" :key="n.id">{{ n }}</li>
+      <li v-for="n in evenNumbers" :key="n">{{ n }}</li>
     </ul>
+    <button @click="this.numbers.push(2)">+</button>
+    <hr />
     <ul v-for="numbers in sets" :key="numbers">
       <li v-for="n in even(numbers)" :key="n">{{ n }}</li>
     </ul>
-    <button @click="sets[0].push(2)">add</button>
+    <button @click="sets[0].push(2)">+</button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .layout {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
