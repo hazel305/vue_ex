524472802ce40454f9178435f9ea731d66bca85e
hazel305 <hazel413@naver.com>
2023-10-26 11:59:02 +0900

    [CREATE]vue todoList setting

---
+++ todolist/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      todoText: "",
+      todos: [
+        { id: 1, text: "learn chapter1", checked: false },
+        { id: 2, text: "learn chapter2", checked: false },
+        { id: 3, text: "learn chapter3", checked: false },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center">todo App</h1>
+    <div class="mb-3">
+      <label for="todo" class="form-label">Todo Input</label>
+      <input
+        type="text"
+        class="form-control"
+        id="todo"
+        placeholder="Add todo"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
